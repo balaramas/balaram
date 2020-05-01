aa22442d1268384d3d42253a9bd7be4a79016ea0
BALARAM SARKAR <balaramsarkar@BALARAMs-MacBook-Air-2.local>
2020-05-01 19:14:57 +0530

    Updates

---
+++ precache-manifest.f2d2bb2be0c9318edb223d8ce6c1f326.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f735f3fa46aecd1b9ee0fad427d563e1",
+    "url": "/balaram/index.html"
+  },
+  {
+    "revision": "2d0ebf2f670e25f9e125",
+    "url": "/balaram/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2e5eaae90b69dca565ff",
+    "url": "/balaram/static/css/main.a7911cec.chunk.css"
+  },
+  {
+    "revision": "2d0ebf2f670e25f9e125",
+    "url": "/balaram/static/js/2.7eddcaa7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/balaram/static/js/2.7eddcaa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e5eaae90b69dca565ff",
+    "url": "/balaram/static/js/main.ff55272f.chunk.js"
+  },
+  {
+    "revision": "201ba98f3ca576992317",
+    "url": "/balaram/static/js/runtime-main.7deda5f7.js"
+  },
+  {
+    "revision": "8970d91f4609b5ccaba603eae6201a65",
+    "url": "/balaram/static/media/bentley.8970d91f.jpg"
+  },
+  {
+    "revision": "ca876a59b1103dd5b0864256120a03c6",
+    "url": "/balaram/static/media/ferrari.ca876a59.jpg"
+  },
+  {
+    "revision": "be32d8026d1fa69190c5a6e3d00fff63",
+    "url": "/balaram/static/media/ima.be32d802.jpg"
+  },
+  {
+    "revision": "b524905ad47297f39e90816971bca3d0",
+    "url": "/balaram/static/media/lamb.b524905a.jpg"
+  },
+  {
+    "revision": "7bac615ca4a9c1e64c229d4bd0e5e3ad",
+    "url": "/balaram/static/media/lambor.7bac615c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/balaram/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f27d119430fc5025bcef298ce1aca544",
+    "url": "/balaram/static/media/lotus.f27d1194.jpg"
+  },
+  {
+    "revision": "5d41245d7401ddf40df096dc2ba16dda",
+    "url": "/balaram/static/media/p1.5d41245d.jpg"
+  },
+  {
+    "revision": "a1033b46a3e89701dfcfef1737b6c88b",
+    "url": "/balaram/static/media/pagani.a1033b46.jpg"
+  },
+  {
+    "revision": "c03dd50da451777508478ae2c9cd7a79",
+    "url": "/balaram/static/media/s1.c03dd50d.jpg"
+  },
+  {
+    "revision": "5a55efae5b2720a0e3b9ab7c3e7c5459",
+    "url": "/balaram/static/media/s2.5a55efae.jpg"
+  },
+  {
+    "revision": "ff7463e972177b8a588678266bf265fa",
+    "url": "/balaram/static/media/s3.ff7463e9.jpg"
+  },
+  {
+    "revision": "5e552bc8679528243a4139b4707a45d5",
+    "url": "/balaram/static/media/s5.5e552bc8.jpg"
+  },
+  {
+    "revision": "5ce8e55b3dc70cc62e2e0269d016cd93",
+    "url": "/balaram/static/media/s6.5ce8e55b.jpg"
+  },
+  {
+    "revision": "1c6f95f191687251732cace1a2f92ae5",
+    "url": "/balaram/static/media/s8.1c6f95f1.jpg"
+  }
+]);
